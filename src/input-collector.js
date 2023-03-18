53a9fa857783c1bee55d99243abd4219d2b9d848
Manon <manonlefkos@gmail.com>
2023-03-18 22:42:17 +0100

    add data collector for new tasks

---
+++ src/input-collector.js
@@ -0,0 +1,12 @@
+function getNewTaskInfo() {
+  const title = "noob";
+  const description = "by noob";
+  const dueDate = "noobier";
+  const priority = "low";
+
+  const details = [title, description, dueDate, priority];
+  return details;
+}
+
+const taskInfo = getNewTaskInfo();
+export default taskInfo
