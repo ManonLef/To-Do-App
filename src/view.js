2f0f6eb7ebe531491a0d1bc13eef5e4a2728ce31
Manon <manonlefkos@gmail.com>
2023-03-20 00:13:21 +0100

    add function to create data array from form

---
+++ src/view.js
@@ -0,0 +1,10 @@
+export default function getTaskFromForm() {
+  const task = document.querySelector("#task").value;
+  const description = document.querySelector("#description").value;
+  const due = document.querySelector("#due-date").value;
+  const prio = document.querySelector("#priority").value;
+  const project = document.querySelector("#project").value;
+
+ return [task, description, due, prio, project];
+}
+
