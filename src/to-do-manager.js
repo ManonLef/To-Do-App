1012668cb037433e5b98482c07bcb23b7dee4d8d
Manon <manonlefkos@gmail.com>
2023-03-20 00:12:55 +0100

    update task function to create class from form

--- src/to-do-manager.js
+++ src/to-do-manager.js
@@ -1,11 +1,10 @@
-import taskInfo from "./input-collector";
 import {
   showProjectsTEST,
   addTaskToProject,
   getProjects,
 } from "./project-manager";
+import getTaskFromForm from "./view"
 
-// todo Class creates default todo with multiple properties
 class Task {
   constructor(task, description, dueDate, priority, project) {
     this.task = task;
@@ -16,81 +15,82 @@ class Task {
   }
 }
 
-// create new task. I want to import the data for it somewhere. Currently I've stored it in the input-collector
-// since I'm not entirely sure yet where it will get its data from and which modules it should travel.
-function createTask(info) {
-  const newTask = new Task(info[0], info[1], info[2], info[3], info[4]);
-  // test
-  addTaskToProject(newTask, info[4]);
-}
-
-//  --------------------------------------------------------------------------
-//  |||||||||||||||||||||||||| • New Functionality • |||||||||||||||||||||||||
-//  --------------------------------------------------------------------------
-
-// displays all tasks regardless of project
 function returnAllTasks() {
-  // example filter1 returnAllTasks(task)
-  // // example filter1 this function could be passed an argument and return only what's asked
   const allProjects = getProjects();
   const allTasks = [];
-  console.log("made it inside displayAllTasks function");
   for (let i = 0; i < allProjects.length; i += 1) {
-    // for each project, display each task in it
     for (let j = 0; j < allProjects[i].projectTasks.length; j += 1) {
-      allTasks.push(allProjects[i].projectTasks[j]); // example filter1 with just title: allTasks.push(allProjects[i].projectTasks[j].task)
+      allTasks.push(allProjects[i].projectTasks[j]); 
     }
   }
   console.table(allTasks);
   return allTasks;
 }
 
+function createTask() {
+  const taskDetails = getTaskFromForm();
+  const newTask = new Task(taskDetails[0], taskDetails[1], taskDetails[2], taskDetails[3], taskDetails[4]);
+  addTaskToProject(newTask, taskDetails[4]);
+  // for testing only
+  showProjectsTEST();
+  returnAllTasks();
+}
+
+document.querySelector("button").addEventListener("click", createTask)
+
+//  --------------------------------------------------------------------------
+//  |||||||||||||||||||||||||| • New Functionality • |||||||||||||||||||||||||
+//  --------------------------------------------------------------------------
+
+
+
 //  --------------------------------------------------------------------------
 //  |||||||||||||||||||||||||||| • Testing area • ||||||||||||||||||||||||||||
 //  --------------------------------------------------------------------------
 
-createTask(taskInfo); // this task has no project assigned and should end up in the inbox
-createTask([
-  "this task should go to inbox",
-  "it's a pretty awesome inbox task",
-  "none",
-  "low",
-  "inbox",
-]);
-createTask([
-  "this will go into the bin project",
-  "it's binny",
-  "tomorrow",
-  "high",
-  "bin",
-]);
-createTask([
-  "hug a tree",
-  "this task is for instagram hug a tree day",
-  "before hug a tree day",
-  "low",
-  "insta",
-]);
-createTask([
-  "insta 2",
-  "this task is for instagram hug a tree day",
-  "before hug a tree day",
-  "low",
-  "insta",
-]);
-createTask([
-  "insta 3",
-  "this task is for instagram hug a tree day",
-  "before hug a tree day",
-  "low",
-  "insta",
-]);
-createTask(["empty project should end in inbox", "", "", "", ""]);
+// createTask(taskInfo); // this task has no project assigned and should end up in the inbox
+// createTask([
+//   "this task should go to inbox",
+//   "it's a pretty awesome inbox task",
+//   "none",
+//   "low",
+//   "inbox",
+// ]);
+// createTask([
+//   "this will go into the bin project",
+//   "it's binny",
+//   "tomorrow",
+//   "high",
+//   "bin",
+// ]);
+// createTask([
+//   "hug a tree",
+//   "this task is for instagram hug a tree day",
+//   "before hug a tree day",
+//   "low",
+//   "insta",
+// ]);
+// createTask([
+//   "insta 2",
+//   "this task is for instagram hug a tree day",
+//   "before hug a tree day",
+//   "low",
+//   "insta",
+// ]);
+// createTask([
+//   "insta 3",
+//   "this task is for instagram hug a tree day",
+//   "before hug a tree day",
+//   "low",
+//   "insta",
+// ]);
+// createTask(["empty project should end in inbox", "", "", "", ""]);
 
-// testing functions
-showProjectsTEST();
-returnAllTasks();
+// // testing functions
+// showProjectsTEST();
+// returnAllTasks();
 
 // // testing the removeProject functionality from project-manager
 // removeProject(0);
 // showProjectsTEST();
+
