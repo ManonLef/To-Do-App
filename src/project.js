8440f8046d6fff950da4c7b986a146ecc573f237
Manon <manonlefkos@gmail.com>
2023-03-24 14:46:49 +0100

    remove obsolete class methods

--- src/project.js
+++ src/project.js
@@ -4,19 +4,8 @@ export default class Project {
     this.projectTasks = [];
   }
 
-  get tasks() {
-    return this.projectTasks;
-  }
-
   set tasks(add) {
     this.projectTasks.push(add);
   }
-
-  get projectName() {
-    return this.name;
-  }
   
-  set projectName(newName) {
-    this.name = newName;
-  }
 }
