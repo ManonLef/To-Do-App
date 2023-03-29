af39a4a7dca77550a4d0fc4f9f9766090afab2fe
Manon <manonlefkos@gmail.com>
2023-03-29 16:27:47 +0200

    add projectForm to DOM and remove from html

--- src/view.js
+++ src/view.js
@@ -1,11 +1,38 @@
 const taskContainer = document.querySelector(".task-container");
 const topContainer = document.querySelector(".container");
-
+const body = document.querySelector("body");
 function removeChildNodes(parent) {
   while (parent.firstChild) {
     parent.removeChild(parent.firstChild);
   }
 }
+
+function projectForm() {
+  const newProjectForm = document.createElement("form");
+
+  const newProjectDiv = document.createElement("div");
+  newProjectDiv.className = "project";
+
+  const label = document.createElement("label");
+  label.setAttribute("for", "project");
+  label.textContent = "project";
+
+  const input = document.createElement("input");
+  input.setAttribute("type", "text");
+  input.setAttribute("name", "project");
+  input.setAttribute("id", "project");
+
+  const submit = document.createElement("button")
+  submit.setAttribute("type", "submit")
+  submit.setAttribute("class", "add-project-form")
+  submit.textContent = "add project noob"
+
+  body.appendChild(newProjectForm)
+  newProjectDiv.append(label, input)
+  newProjectForm.append(newProjectDiv, submit)
+}
+
+projectForm()
 
 function createProjectElements(currentProject) {
   const projectDiv = document.createElement("div");
@@ -16,7 +43,7 @@ function createProjectElements(currentProject) {
   projectName.className = "project-name";
   projectName.textContent = currentProject.name;
   projectName.setAttribute("data-projectID", currentProject.projectUuid);
-  projectDiv.appendChild(projectName)
+  projectDiv.appendChild(projectName);
 
   const editButton = document.createElement("button");
   editButton.className = "project-edit-button";
@@ -85,5 +112,5 @@ function renderProjects(vaultProjectsArray) {
 
 export default function renderCurrent(allProjects, currentProjectTasks) {
   renderProjects(allProjects);
-  renderTasks(currentProjectTasks)
+  renderTasks(currentProjectTasks);
 }
