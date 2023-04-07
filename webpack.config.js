701c12f0bdf684e5cb53f576cc174ee9ad56dfbc
Manon <manonlefkos@gmail.com>
2023-04-07 16:56:01 +0200

    add asset loader

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,11 @@ module.exports = {
       {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
-      }
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
     ]
   }
 };
