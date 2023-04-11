766308c9179f4eb0d0a08a1894b85e80cd7d16aa
Manon <manonlefkos@gmail.com>
2023-04-11 16:58:02 +0200

    update file loader

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,9 @@ module.exports = {
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
+        use: [
+          'file-loader',
+        ],
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
